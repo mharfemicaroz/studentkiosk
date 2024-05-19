7a80ecb2ef7d82c9bd9bc05e0f74d0f7abdb3422
unknown <mharfemicaroz@gmail.com>
2024-05-19 18:32:09 +0800

    fix https

--- kiosk/src/services/profileServices.js
+++ kiosk/src/services/profileServices.js
@@ -4,7 +4,7 @@ let BASE_URL;
 if (window.location.hostname === "kiosk.ndci.edu.ph") {
   BASE_URL = `https://ndci-api.portmap.host:20300/api/`;
 } else {
-  BASE_URL = `http://${window.location.hostname}:20300/api/`;
+  BASE_URL = `https://${window.location.hostname}:20300/api/`;
 }
 
 export const getStudentById = async (id) => {

b160156cd7b666ffaf54d2fe879d8bed898c84c5
unknown <mharfemicaroz@gmail.com>
2024-05-19 14:35:32 +0800

    build dist

--- kiosk/vite.config.js
+++ kiosk/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -13,10 +12,19 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[hash].[ext]",
+        chunkFileNames: "[name].[hash].js",
+        entryFileNames: "[name].[hash].js",
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "./",
+  base: "/", // Ensure this is correct for your deployment environment
 });

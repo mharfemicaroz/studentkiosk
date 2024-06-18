8648a13c5724a4c3b0537b1415a8176348c0cdd2
unknown <mharfemicaroz@gmail.com>
2024-06-19 06:57:36 +0800

    new port

--- kiosk/src/services/profileServices.js
+++ kiosk/src/services/profileServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let BASE_URL;
 if (window.location.hostname === "kiosk.ndci.edu.ph") {
-  BASE_URL = `https://ndci-api.portmap.io:43529/api/`;
+  BASE_URL = `https://ndci-api.portmap.io:29956/api/`;
 } else {
   BASE_URL = `https://${window.location.hostname}:25856/api/`;
 }

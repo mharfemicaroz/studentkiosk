15b9ff051338d3a1847300d136a25a11ee2573e3
unknown <mharfemicaroz@gmail.com>
2025-03-10 11:28:30 +0800

    changes

--- kiosk/src/main.js
+++ kiosk/src/main.js
@@ -9,9 +9,13 @@ import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.min.css";
 
 const app = createApp(App);
+
 app.config.globalProperties = {
   API_URL: `http://${window.location.hostname}:8081/api/`,
 };
+
+// Set dynamic title from environment variables
+document.title = import.meta.env.VITE_APP_TITLE || "Default Title";
 
 app.use(router);
 app.use(createPinia());
